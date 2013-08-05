9ea1e6530acb3cc7c5e9b4e5750dec1e6a7a1954
Michael Mulich <michael.mulich@gmail.com>
2013-08-05 15:58:57 -0400

    A testing skeleton that includes a fixture for working against the cnx-archive database. This utilizes the cnx-archive library for initializing the database structure in the testing fixture.

---
+++ cnxupgrade/tests/test_upgrades.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2013, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+import os
+import sys
+import unittest
+
+import psycopg2
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+_DB_CONNECTION_STRING_ENV_VAR_NAME = 'DB_CONNECTION_STRING'
+_DB_CONNECTION_STRING_CLI_OPT_NAME = '--db-conn-str'
+try:
+    DB_CONNECTION_STRING = os.environ[_DB_CONNECTION_STRING_ENV_VAR_NAME]
+except:
+    try:
+        arg_pos = sys.argv.index(_DB_CONNECTION_STRING_CLI_OPT_NAME)
+    except ValueError:
+        raise RuntimeError("MUST supply a means to connect to the database, "
+                           "either via the environment variable '{}' or the "
+                           "command-line option '{}'." \
+                               .format(_DB_CONNECTION_STRING_ENV_VAR_NAME,
+                                       _DB_CONNECTION_STRING_CLI_OPT_NAME)
+                           )
+    DB_CONNECTION_STRING = sys.argv[arg_pos+1]
+
+
+class PostgresqlFixture:
+    """A testing fixture for a live (same as production) SQL database.
+    This will set up the database once for a test case. After each test
+    case has completed, the database will be cleaned (all tables dropped).
+
+    On a personal note, this seems archaic... Why can't I rollback to a
+    transaction?
+    """
+
+    def __init__(self):
+        # Configure the database connection.
+        self._connection_string = DB_CONNECTION_STRING
+        # Drop all existing tables from the database.
+        self._drop_all()
+
+    def _drop_all(self):
+        """Drop all tables in the database."""
+        with psycopg2.connect(self._connection_string) as db_connection:
+            with db_connection.cursor() as cursor:
+                cursor.execute("DROP SCHEMA public CASCADE")
+                cursor.execute("CREATE SCHEMA public")
+
+    def setUp(self):
+        # Initialize the database schema.
+        from cnxarchive.database import initdb
+        settings = {'db-connection-string': self._connection_string}
+        initdb(settings)
+
+    def tearDown(self):
+        # Drop all tables.
+        self._drop_all()
+
+postgresql_fixture = PostgresqlFixture()
+
+
+class ToHtmlTestCase(unittest.TestCase):
+    fixture = postgresql_fixture
+
+    def test_something(self):
+        pass
--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 
 
 install_requires = (
+    'psycopg2',
     )
+tests_require = ('cnx-archive',)
+extras_require = {
+    'tests': tests_require
+    }
 description = "An upgrade utility"
 
 
@@ -17,10 +22,12 @@ setup(
     description=description,
     packages=find_packages(),
     install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
     include_package_data=True,
     entry_points="""\
     [console_scripts]
     cnx-upgrade = cnxupgrade.cli:main
     """,
-    # test_suite=''
+    test_suite='cnxupgrade.tests',
     )
