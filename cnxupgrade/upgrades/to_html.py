3743e650bc40341bcd6e4e8da2b7f4a7c592d769
Michael Mulich <michael.mulich@gmail.com>
2013-08-21 21:39:23 -0400

    Move some logic around and change the StringIO usage to BytesIO.

--- cnxupgrade/upgrades/to_html.py
+++ cnxupgrade/upgrades/to_html.py
@@ -7,10 +7,7 @@
 # ###
 """Upgrades for munging/transforming Connexions XML formats to HTML."""
 import os
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+from io import BytesIO
 
 import rhaptos.cnxmlutils
 from lxml import etree
@@ -33,81 +30,6 @@ HTML_TEMPLATE_FOR_CNXML = """\
 """
 RHAPTOS_CNXMLUTILS_DIR = os.path.dirname(rhaptos.cnxmlutils.__file__)
 XSL_DIRECTORY = os.path.abspath(os.path.join(RHAPTOS_CNXMLUTILS_DIR, 'xsl'))
-
-
-def transform_cnxml_to_html(cnxml):
-    """Transforms raw cnxml content to html."""
-    gen_xsl = lambda f: etree.XSLT(etree.parse(f))
-    cnxml = etree.parse(StringIO(cnxml))
-
-    # Transform the content to html.
-    cnxml_to_html_filepath = os.path.join(XSL_DIRECTORY, 'cnxml-to-html5.xsl')
-    cnxml_to_html = gen_xsl(cnxml_to_html_filepath)
-    content = cnxml_to_html(cnxml)
-
-    # Transform the metadata to html.
-    cnxml_to_html_metadata_filepath = os.path.join(
-            XSL_DIRECTORY,
-            'cnxml-to-html5-metadata.xsl')
-    cnxml_to_html_metadata = gen_xsl(cnxml_to_html_metadata_filepath)
-    metadata = cnxml_to_html_metadata(cnxml)
-
-    html = HTML_TEMPLATE_FOR_CNXML.format(metadata=metadata, content=content)
-    return html
-
-
-def transform_collxml_to_html(collxml):
-    """Transforms raw collxml content to html"""
-    # XXX Temporarily return the same thing, worry about the transform
-    #     after the higher level process works through.
-    html = collxml
-    return html
-
-
-def produce_html_for_collections(db_connection):
-    """Produce HTML files of existing collection documents. This will
-    do the work on all collections in the database.
-
-    Yields a state tuple after each collection is handled.
-    The state tuple contains the id of the collection that was transformed
-    and either None when no errors have occured
-    or a message containing information about the issue.
-    """
-    with db_connection.cursor() as cursor:
-        cursor.execute("SELECT module_ident FROM modules "
-                       "  WHERE portal_type = 'Collection';")
-        # Note, the "ident" is different from the "id" in our tables.
-        collection_idents = cursor.fetchall()
-
-    for collection_ident in collection_idents:
-        with db_connection.cursor() as cursor:
-            # FIXME There is a better way to join this information, but
-            #       for the sake of testing scope stick with the simple yet
-            #       redundant lookups.
-            cursor.execute("SELECT filename, fileid FROM module_files "
-                           "  WHERE module_ident = %s;", (collection_ident,))
-            file_metadata = dict(cursor.fetchall())
-            file_id = file_metadata['collection.xml']
-            # Grab the file for transformation.
-            cursor.execute("SELECT file FROM files WHERE fileid = %s;",
-                           (file_id,))
-            collxml = cursor.fetchone()[0]
-            collxml = collxml[:]
-            collection_html = transform_collxml_to_html(collxml)
-            # Insert the collection.html into the database.
-            payload = (Binary(collection_html),)
-            cursor.execute("INSERT INTO files (file) VALUES (%s) "
-                           "RETURNING fileid;", payload)
-            collection_html_file_id = cursor.fetchone()[0]
-            cursor.execute("INSERT INTO module_files "
-                           "  (module_ident, fileid, filename, mimetype) "
-                           "  VALUES (%s, %s, %s, %s);",
-                           (collection_ident, collection_html_file_id,
-                            'collection.html', 'text/html',))
-        yield (collection_ident, None)
-
-    raise StopIteration
-
 
 SQL_MODULE_ID_TO_MODULE_IDENT = """\
 SELECT module_ident FROM modules
@@ -204,6 +126,80 @@ def fix_reference_urls(db_connection, document_ident, html):
         anchor.set('href', '/contents/{}@{}'.format(uuid, version))
 
     return etree.tostring(xml_doc)
+
+
+def transform_cnxml_to_html(cnxml):
+    """Transforms raw cnxml content to html."""
+    gen_xsl = lambda f: etree.XSLT(etree.parse(f))
+    cnxml = etree.parse(BytesIO(cnxml))
+
+    # Transform the content to html.
+    cnxml_to_html_filepath = os.path.join(XSL_DIRECTORY, 'cnxml-to-html5.xsl')
+    cnxml_to_html = gen_xsl(cnxml_to_html_filepath)
+    content = cnxml_to_html(cnxml)
+
+    # Transform the metadata to html.
+    cnxml_to_html_metadata_filepath = os.path.join(
+            XSL_DIRECTORY,
+            'cnxml-to-html5-metadata.xsl')
+    cnxml_to_html_metadata = gen_xsl(cnxml_to_html_metadata_filepath)
+    metadata = cnxml_to_html_metadata(cnxml)
+
+    html = HTML_TEMPLATE_FOR_CNXML.format(metadata=metadata, content=content)
+    return html
+
+
+def transform_collxml_to_html(collxml):
+    """Transforms raw collxml content to html"""
+    # XXX Temporarily return the same thing, worry about the transform
+    #     after the higher level process works through.
+    html = collxml
+    return html
+
+
+def produce_html_for_collections(db_connection):
+    """Produce HTML files of existing collection documents. This will
+    do the work on all collections in the database.
+
+    Yields a state tuple after each collection is handled.
+    The state tuple contains the id of the collection that was transformed
+    and either None when no errors have occured
+    or a message containing information about the issue.
+    """
+    with db_connection.cursor() as cursor:
+        cursor.execute("SELECT module_ident FROM modules "
+                       "  WHERE portal_type = 'Collection';")
+        # Note, the "ident" is different from the "id" in our tables.
+        collection_idents = cursor.fetchall()
+
+    for collection_ident in collection_idents:
+        with db_connection.cursor() as cursor:
+            # FIXME There is a better way to join this information, but
+            #       for the sake of testing scope stick with the simple yet
+            #       redundant lookups.
+            cursor.execute("SELECT filename, fileid FROM module_files "
+                           "  WHERE module_ident = %s;", (collection_ident,))
+            file_metadata = dict(cursor.fetchall())
+            file_id = file_metadata['collection.xml']
+            # Grab the file for transformation.
+            cursor.execute("SELECT file FROM files WHERE fileid = %s;",
+                           (file_id,))
+            collxml = cursor.fetchone()[0]
+            collxml = collxml[:]
+            collection_html = transform_collxml_to_html(collxml)
+            # Insert the collection.html into the database.
+            payload = (Binary(collection_html),)
+            cursor.execute("INSERT INTO files (file) VALUES (%s) "
+                           "RETURNING fileid;", payload)
+            collection_html_file_id = cursor.fetchone()[0]
+            cursor.execute("INSERT INTO module_files "
+                           "  (module_ident, fileid, filename, mimetype) "
+                           "  VALUES (%s, %s, %s, %s);",
+                           (collection_ident, collection_html_file_id,
+                            'collection.html', 'text/html',))
+        yield (collection_ident, None)
+
+    raise StopIteration
 
 
 def produce_html_for_modules(db_connection):
