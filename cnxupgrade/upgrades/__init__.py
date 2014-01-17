0f122779e0239ce753ea0075934d35e456f9a187
karen chan <karen@karen-chan.com>
2014-01-17 20:33:17 +0100

    Add script to migrate google analytics code from legacy

--- cnxupgrade/upgrades/__init__.py
+++ cnxupgrade/upgrades/__init__.py
@@ -33,6 +33,7 @@ UPGRADES = (
     'cnxml7',
     'create_collection_minor_versions',
     'remove_testdraft',
+    'migrate_ga',
     )
 # TODO Look this up via setuptools entry-point so that it only needs to be
 #      changed at the distribution level on say release or tag.
